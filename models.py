fb8d42a4c45506e2ca3ef13ddf84abc22360d104
Kvaster87 <ruslancernuy8@gmail.com>
2026-08-17 17:02:09 +0200

    added type hints

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 class Expense:
-    def __init__(self, name, price, category):
+    def __init__(self, name: str, price: int, category: str):
         self.name = name
         self.price = price
         self.category = category
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name} - {self.price}$ in -{self.category}- category\n"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Expense(name='{self.name}', price={self.price}, category='{self.category}')"
 
-    def to_file_line(self):
+    def to_file_line(self) -> str:
         return f"{self.name};{self.price};{self.category}\n"
--- storage.py
+++ storage.py
@@ -1,6 +1,6 @@
 from models import Expense
 
-def load_expense():
+def load_expense() -> list[Expense]:
     expenses = []
     with open("data/expenses.txt", 'r', encoding='utf-8') as f:
         for line in f.readlines():
@@ -9,9 +9,9 @@ def load_expense():
             expenses.append(Expense(name, int(price), category))
     return expenses
 
-def save_expense(expense):
+def save_expense(expense) -> None:
     with open("data/expenses.txt", 'a', encoding='utf-8') as f:
         f.write(expense.to_file_line())
 
-def get_categories(expenses):
+def get_categories(expenses: list[Expense]) -> set[str]:
     return {e.category for e in expenses}
