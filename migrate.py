e58cde57a11cd3a92ab6a20576bdfaf79b6a0551
Kvaster87 <ruslancernuy8@gmail.com>
2026-09-09 14:38:22 +0200

    Maked a migrate function for new format

---
+++ migrate.py
@@ -0,0 +1,28 @@
+import uuid
+
+Old_format_fields = 4
+
+def migrate():
+
+    with open('data/expenses.json', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    new_lines = []
+    for line in lines:
+        parts = line.split(',')
+
+        if len(parts) == Old_format_fields:
+            user_id, name, price, category = parts
+            expense_id = uuid.uuid4()
+            new_lines.append(f"{expense_id};{user_id};{name},{price},{category}")
+
+        else:
+            new_lines.append(line)
+
+    with open('data/expenses.txt', 'w', encoding='utf-8') as f:
+        f.writelines(new_lines)
+
+    print(f"Migration was successful! \nNew lines: {len(new_lines)}")
+
+if __name__ == "__main__":
+    migrate()
